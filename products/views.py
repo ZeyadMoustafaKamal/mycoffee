05d78d9bca80916c5f250fb45c1d734802c17cf6
ZeyadMoustafaKamal <zeyad20062018@gmail.com>
2023-10-16 21:49:37 +0300

    Main improvements

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 from django_filters.views import FilterView
 
 from accounts.models import UserProfile
@@ -8,6 +8,8 @@ from core.mixins import HTMXTemplateMixin
 
 from .models import Product
 from .filters import ProductFilter
+
+User = get_user_model()
 
 class ListProductsView(HTMXTemplateMixin, FilterView):
     model = Product
@@ -18,26 +20,42 @@ class ListProductsView(HTMXTemplateMixin, FilterView):
 
 
 def product_details(request, pk):
-    is_favourite = False
+    """
+        TODO: Optimize the performance here (even if I do think that it can't be optimized more)
+    """
+    profile: UserProfile = UserProfile.objects.prefetch_related('favourites').get(user=request.user) \
+        if request.user.is_authenticated else None
     product = get_object_or_404(Product, pk=pk)
+    
+    # I don' want to know if the product is in the favourite list or not if "favourite" in the POST data
+    # as I will know this in the toggle_product method of the UserProfile model below 
+    is_favourite = False if request.user.is_anonymous or 'favourite' in request.POST else \
+        product in profile.favourites.all()
+    # I am doing this in a different way as I see alot of people do something like this
+    # if request.method == 'POST' and 'favourite' in request.POST
+    # and I think that it will be better to just say "if 'favourite' in request.POST"
+    # Because if the request is not POST the request.POST will return something like an empty dict (its actually an extended dict)
+    if 'favourite' in request.POST and request.user.is_authenticated:
+        is_favourite = profile.toggle_product(product)
     context = {
         'product':product,
         'is_favourite':is_favourite
     }
-    if request.user.is_authenticated:
-        profile = UserProfile.objects.prefetch_related('favourites').get(pk=request.user.pk)
-        is_favourite = product in profile.favourites.all()
-        if request.method == 'POST' and 'favourite' in request.POST:
-            is_favourite = profile.toggle_product(product)
-            print(is_favourite)
-        context['is_favourite'] = is_favourite
     return render_htmx(
         request, 
         'products/details.html', 
         'products/parts/_details.html', 
         context
     )
+
 def search_view(request):
     filter = ProductFilter()
-    return render(request, 'products/search.html', {'filter':filter})
+    return render_htmx(
+        request,
+        'product/search.html',
+        'products/parts/_search.html',
+        {
+            'filter':filter
+        }
+    )
 
