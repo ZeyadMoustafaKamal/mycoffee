a1a782ec99b5a07b8a1d1dd5352f3abe7b7dd276
ZeyadMoustafaKamal <zeyad20062018@gmail.com>
2023-10-22 21:41:52 +0300

    Use built-in dumpdata command instead of a custom one

--- products/management/commands/load_data.py
+++ products/management/commands/load_data.py
@@ -6,6 +6,9 @@ import random
 import requests
 
 class Command(BaseCommand):
+    """ I was using this command before and every time I build the image I get the products again but this is really bad practice
+        so I decided to make a fixture for this and just load the data using that fixure but I will still let this here. 
+    """
     def handle(self, *args, **options):
         Product.objects.all().delete() # Delete all products and then get them again
         url = 'https://api.sampleapis.com/coffee/hot/'
